edabc7bde5f0450fcda166dc20fd3f264511f4ac
HaUma-123 <d16662536@gmail.com>
2024-06-10 23:13:30 +0300

    Create creepy.js

---
+++ scripts/cmds/creepy.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+module.exports = {
+    config: {
+        name: "creepy",
+        aliases: ['horror', 'spooky'],
+        author: "Hassan",
+        version: "1.0",
+        shortDescription: "Get a creepy fact",
+        longDescription: "Retrieve a random creepy fact to send shivers down your spine.",
+        category: "fun",
+        guide: {
+            vi: "",
+            en: ""
+        }
+    },
+
+    onStart: async function ({ message }) {
+        try {
+            const url = 'http://numbersapi.com/random/trivia';
+            message.reply('🔍 Delving into the darkness🌌 for a fact...');
+
+            const response = await axios.get(url);
+            const fact = response.data;
+
+            return message.reply(`📜 Here's a creepy fact:\n\n"${fact}"`);
+        } catch (error) {
+            console.error(error);
+            return message.reply("🚫 The shadows are being uncooperative. There was an error fetching the creepy fact.");
+        }
+    }
